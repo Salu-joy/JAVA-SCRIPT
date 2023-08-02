8993e83e33d6056c8dc7070bbe90addb85b84726
Salu-joy <Salinibalasubramanian@gmail.com>
2023-08-02 12:59:22 +0530

    java Script data types

---
+++ JS/script.js
@@ -0,0 +1,60 @@
+console.log();
+console.log(2 + 10);
+console.log("Hello World!!");
+console.log(2 / 10);
+console.log(2 * 10);
+console.log(1, 2, 2, 44);
+alert("hello");
+console.log("Hello World!!");
+2 + 5;
+let numbo = 2;
+let numbtwo = 3;
+console.log(numbo * numbtwo);
+let stname =
+  "salu this is text so inside quotaion la irukanum otherwise ouput varathu";
+console.log(stname);
+
+let noise = "salu";
+console.log(noise);
+console.log(typeof noise);
+noise = 1234;
+console.log(noise);
+console.log(typeof noise);
+noise = true;
+console.log(noise);
+console.log(typeof noise);
+
+let name = "john";
+let firstName = "john";
+let lastName = "jack";
+let dateOfBirth = "12-01-2020";
+console.log(noise + firstName + lastName + dateOfBirth);
+
+let age = 23;
+console.log(age);
+console.log(typeof age);
+
+let bigNum = 908230948230948230948230948230948213094812309n;
+console.log(bigNum);
+console.log(typeof bigNum);
+
+let yoga = "yoga";
+console.log(yoga);
+let daya = "daya";
+console.log(daya);
+let yogaAge = 7 + 10;
+let dayaAge = `10`;
+console.log(yogaAge);
+console.log(yoga + "" + daya + yogaAge);
+
+console.log(yoga + "" + daya + yogaAge + 23 + dayaAge);
+console.log(`daya,${5 + 5}`);
+
+let venkat = null;
+console.log(venkat);
+
+let Thiru;
+console.log(Thiru);
+
+let admin = 54 > 23;
+console.log(54 > 23);
